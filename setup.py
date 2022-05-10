039c1ad6033d28e3a420ba86c0de41a619250558
gnifajio <gnifajio@gmail.com>
2022-05-10 13:54:58 +0300

    modifed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
-
-setup(
-    name='gQiwiAPI',
-    version='1.1',
-    packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README_EN.md')).read(),
-)
-
-
 import setuptools
 
 with open('README_EN.md', 'r') as fh:
